c28c061028d0c6b645336793d936de54173b9acb
Erol Bicer <erolbicer.subs@gmail.com>
2022-08-03 21:41:13 +0900

    finished until contact me @1:50

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
     
@@ -109,7 +109,22 @@ let swiper = new Swiper('.portfolio__container', {
   });
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
